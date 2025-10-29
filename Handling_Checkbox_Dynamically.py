35e77eee9cc5ebde44d73270fde8f3bbf0b5f796
Sai Ratna MUKTA <mukta.sairatna1@gmail.com>
2025-10-29 10:22:29 +0530

    Handling checkbox dyanmically by get_attribute('value')

---
+++ Handling_Checkbox_Dynamically.py
@@ -0,0 +1,13 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+checkboxes = driver.find_elements(By.XPATH, "//input[@type='checkbox']")
+for checkbox in checkboxes:
+    if checkbox.get_attribute('value') == "option2":
+        checkbox.click()
+        checkbox.is_selected()
+        break
+time.sleep(5)
